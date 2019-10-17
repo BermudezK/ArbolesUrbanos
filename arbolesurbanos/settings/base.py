04739771e7388c62d693eee27c87ecd678535884
Rodrigo Cerdan <cerdanrodrigo.utn@gmail.com>
2019-10-17 00:26:10 -0300

    Se agrego la app registration para extender el login de Auth

--- arbolesurbanos/settings/base.py
+++ arbolesurbanos/settings/base.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.usuario',
+    
 ]
 
 MIDDLEWARE = [
@@ -114,3 +116,6 @@ STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), "static"),)
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 MEDIA_URL = '/media/'
+
+#Auth redirect
+LOGIN_REDIRECT_URL = 'home'
