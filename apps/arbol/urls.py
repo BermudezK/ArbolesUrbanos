ee290120f3440bb7d1702eba0fc59e787300b432
Raizu548 <rizzoluis53@gmail.com>
2019-10-19 21:12:29 -0300

    ArbolesArboles error

--- apps/arbol/urls.py
+++ apps/arbol/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 	
 	path('Arboles/',views.show_tree, name = 'show_tree'),
 	path('nuevo/',views.nuevo.as_view(), name = 'nuevo'),
+	path('editar/',views.edit_tree.as_view(), name = 'edit_tree'),
 	
 ]
