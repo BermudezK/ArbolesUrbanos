a2ca7ac24ab5488486d3cd73308c7bd15bcd83e2
Rodrigo Cerdan <cerdanrodrigo.utn@gmail.com>
2019-10-23 15:40:47 -0300

    Se actualizo el SignUpForm para su uso con bootstrap

--- apps/usuario/forms.py
+++ apps/usuario/forms.py
@@ -1,21 +1,66 @@
 #Formulario extendido de UserCreationForm
 
 from django import forms
+from apps.usuario.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 #from django.contrib.auth.models import User
 
-class UserCreateFormWithEmail(UserCreationForm):
-    email = forms.EmailField(required=True, help_text="Campo obligatorio. Maximo de 250 caracteres")
-    cellphone = forms.CharField(max_length = 17)
-    birthday = forms.DateField()
+class SignUpForm(UserCreationForm):
+
+    username =forms.EmailField(label="Nombre de Usuario", required=True, widget=forms.EmailInput(
+        attrs={'class':'form-control', 'placeholder':'Escribe tu usuario'}
+    ), min_length=3, max_length=100) 
+    
+    first_name=forms.CharField(label="Nombre", required=True, widget=forms.TextInput(
+        attrs={'class':'form-control', 'placeholder':'Escribe tu Nombre'}
+    ), min_length=3, max_length=30)
+
+    last_name=forms.CharField(label="Apellido", required=True, widget=forms.TextInput(
+        attrs={'class':'form-control', 'placeholder':'Escribe tu Apellido'}
+    ), min_length=3, max_length=30)
+
+    email = forms.EmailField(label="Email", required=True, widget=forms.EmailInput(
+        attrs={'class':'form-control', 'placeholder':'Escribe tu Email'}
+    ), min_length=3, max_length=100)
+
+    cellphone = forms.CharField(label="Celular", required=True, widget=forms.TextInput(
+        attrs={'class':'form-control', 'placeholder':'Escribe tu celular'}
+    ), min_length=3, max_length=30)
+    
+    birthday = forms.DateField(label="Fecha de Nacimiento", required=True, widget=forms.DateInput(
+      attrs={'class':'form-control', 'rows':2, 'placeholder':'Escribe tu fecha de nacimiento'}  
+    ))
+
+    password1 = forms.CharField(required=True, widget=forms.PasswordInput(
+      attrs={'class':'form-control', 'rows':2, 'placeholder':'Escribe tu contraseña'}  
+    ), min_length=5, max_length=25)
+
+    password2 = forms.CharField(required=True, widget=forms.PasswordInput(
+      attrs={'class':'form-control', 'rows':2, 'placeholder':'Repite tu contraseña'}  
+    ), min_length=5, max_length=25)
 
     class Meta:
-        model = User
-        fields = ('username', 'email', 'cellphone', 'birthday', 'password1', 'password2')
+        model=User
+        fields = '__all__'
+        
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+
+        user.username = self.cleaned_data["username"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.email = self.cleaned_data["email"]
+        user.cellphone = self.cleaned_data["cellphone"]
+        user.birthday = self.cleaned_data["birthday"]
+        user.password1 = self.cleaned_data["password1"]
+        user.password2 = self.cleaned_data["password2"]
+        if commit:
+            user.save()
+        return user
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("El email ya se encuentra registrando, intente nuevamente.")
         return email
+
--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, FormView
 from .models import User
 from django.urls import reverse_lazy
-from django import forms
-from .forms import UserCreateFormWithEmail
+from .forms import SignUpForm
+#from .forms import UserCreateFormWithEmail
 
 
 class UpdateUser(UpdateView):
@@ -11,9 +11,9 @@ class UpdateUser(UpdateView):
 	template_name = ''
 	fields = ['password', 'birthday', 'cellphone', 'first_name', 'last_name']
 
-class SignUpView(CreateView):
-    form_class = UserCreateFormWithEmail
+class SignUpView(FormView):
     template_name = 'User/registerUser.html'
+    form_class = SignUpForm
 
     def get_success_url(self):
         return reverse_lazy('login') + '?register'
@@ -21,7 +21,7 @@ class SignUpView(CreateView):
     def get_form(self, form_class=None):
         form = super(SignUpView, self).get_form()
         #Modificar en tiempo real
-        form.fields['username'].widget = forms.TextInput(attrs={'class':'form-control mb-2','placeholder':'Nombre de Usuario'})
+        ''' form.fields['username'].widget = forms.TextInput(attrs={'class':'form-control mb-2','placeholder':'Nombre de Usuario'})
 
         form.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control mb-2', 'placeholder':'Dirección de Email'})
 
@@ -31,6 +31,6 @@ class SignUpView(CreateView):
 
         form.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2',' placeholder':'Contraseña'})
 
-        form.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2',' placeholder':'Repite la contraseña'})
+        form.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2',' placeholder':'Repite la contraseña'}) '''
 
         return form
