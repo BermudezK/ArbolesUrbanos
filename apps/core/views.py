95083512b9a58b3768e74a60683ad9a42e6c5f21
Rodrigo Cerdan <cerdanrodrigo.utn@gmail.com>
2019-10-23 02:39:23 -0300

    Modulo Finalizado

--- apps/core/views.py
+++ apps/core/views.py
@@ -8,39 +8,6 @@ from .forms import ContactForm
 def Inicio (request):
     return render (request, 'core/home.html')
 
-def contacto (request):
-    contact_form = ContactForm()
-
-    if request.method == "POST":
-        contact_form = ContactForm(data=request.POST)
-        if contact_form.is_valid():
-            name = request.POST.get('name', '')
-            surname = request.POST.get('surname', '')
-            email = request.POST.get('email', '')
-            subject = request.POST.get('subject', '')
-            content = request.POST.get('content', '')
-
-            #Enviamos el correo y redireccionamos
-            email = EmailMessage(
-                "Arboles Urbanos: Nuevo mensaje de contacto", #asunto
-                "De {} <{}>\n\nEscribio:\n\n{}".format(name,email,content), #cuerpo 
-                "no-contestar@inbox.mailtrap.io", #email_origen
-                ["softwaremovement19@gmail.com"], #email_destino
-                reply_to=[email]
-            )
-            
-            #Suponemos que todo ha ido bien, entonces redireccionamos
-            #return redirect(reverse('contact:contact')+"?ok")
-            try:
-                "Redireccionamos a OK"
-                email.send()
-                return redirect(reverse('contacto')+"?ok")
-            except:
-                "Redireccionamos a FAIL"
-                return redirect(reverse('contacto')+"?fail")
-
-    return render(request, 'core/contact.html',{'form':contact_form})
-
 def denuncia (request):
     contact_form = ContactForm()
 
@@ -55,9 +22,9 @@ def denuncia (request):
 
             #Enviamos el correo y redireccionamos
             email = EmailMessage(
-                "Arboles Urbanos: Nuevo mensaje de contacto", #asunto
-                "De {} <{}>\n\nEscribio:\n\n{}".format(name,email,content), #cuerpo 
-                "no-contestar@inbox.mailtrap.io", #email_origen
+                "Arboles Urbanos: Nueva Denuncia", #asunto
+                "De: {} <{}>\n\n Asunto: {}\n\n Escribio:\n {}".format(name,email,subject,content), #cuerpo 
+                "{}".format(email), #email_origen
                 ["softwaremovement19@gmail.com"], #email_destino
                 reply_to=[email]
             )
@@ -73,5 +40,48 @@ def denuncia (request):
                 return redirect(reverse('denuncia')+"?fail")
 
     return render(request, 'core/denuncia.html',{'form':contact_form})
+
+def contacto (request):
+    contact_form = ContactForm()
+
+    data={
+        'name': request.user.first_name,
+        'surname': request.user.last_name,
+        'email': request.user.email,
+        'subject': request.POST.get('subject', ''),
+        'content': request.POST.get('content', ''),
+    }
+
+    if request.method == "POST":
+        contact_form = ContactForm(data)
+        if contact_form.is_valid():
+            name = data.get('name', '')
+            surname = data.get('surname', '')
+            email = data.get('email', '')
+            subject = data.get('subject', '')
+            content = data.get('content', '')
+
+            #Enviamos el correo y redireccionamos
+            email = EmailMessage(
+                "Arboles Urbanos: Nuevo mensaje de contacto", #asunto
+                "De: {} <{}>\n\n Asunto: {}\n\n Escribio:\n {}".format(name,email,subject,content), #cuerpo 
+                "{}".format(email), #email_origen
+                ["softwaremovement19@gmail.com"], #email_destino
+                reply_to=[email]
+            )
+            
+            #Suponemos que todo ha ido bien, entonces redireccionamos
+            #return redirect(reverse('contact:contact')+"?ok")
+            try:
+                "Redireccionamos a OK"
+                email.send()
+                return redirect(reverse('contacto')+"?ok")
+            except:
+                "Redireccionamos a FAIL"
+                return redirect(reverse('contacto')+"?fail")
+
+    return render(request, 'core/contacto.html',{'form':contact_form})
+
+
 
 
