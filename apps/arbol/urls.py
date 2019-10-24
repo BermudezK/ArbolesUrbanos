f7ac18225010d1410453f6ba7853a53741f6372e
Raizu <rizzoli548luis@outlook.com>
2019-10-24 15:51:26 -0300

    Vistas y urls terminadas

--- apps/arbol/urls.py
+++ apps/arbol/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
 	
 	path('Arboles/',views.show_tree, name = 'show_tree'),
 	path('nuevo/',views.nuevo.as_view(), name = 'nuevo'),
-	path('editar/<int:pk>/',views.edit_tree.as_view(), name = 'edit_tree'),
+	path('editar/',views.edit_tree, name = 'edit_tree'),
 	
 ]
--- apps/arbol/views.py
+++ apps/arbol/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView, CreateView
+from django.shortcuts import redirect
 
 from .models import Tree
 # Create your views here.
@@ -22,11 +23,15 @@ def show_tree(request):
 	return render(request,'arbol/visualizacion.html',context)
 
 
-class edit_tree(UpdateView):
-	model = Tree
-	success_url = reverse_lazy('arbol:show_tree')
-	fields = '__all__'
-	
+
+
+def edit_tree(request):
+	if request.POST:
+		qty = int(request.POST.get("quantity"))
+		arbol = Tree.objects.get(pk = request.POST.get("pk"))
+		arbol.quantity += qty
+		arbol.save()
+	return redirect('arbol:show_tree')
 
 
 class nuevo(CreateView):
