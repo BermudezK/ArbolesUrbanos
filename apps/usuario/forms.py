8787e52b4da978e5cc1b5ee7998b3fdf29bf0fdc
sandinosanchez <sandino320@gmail.com>
2019-10-24 14:32:27 -0300

    form y register solucionado

--- apps/usuario/forms.py
+++ apps/usuario/forms.py
@@ -2,20 +2,30 @@
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-#from django.contrib.auth.models import User
+from .models import User
 
 class UserCreateFormWithEmail(UserCreationForm):
-    email = forms.EmailField(required=True, help_text="Campo obligatorio. Maximo de 250 caracteres")
-    cellphone = forms.CharField(max_length = 17)
-    birthday = forms.DateField()
+	email = forms.EmailField(required=True, help_text="Campo obligatorio. Maximo de 250 caracteres")
+	cellphone = forms.CharField(max_length = 17)
+	birthday = forms.DateField()
 
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'cellphone', 'birthday', 'password1', 'password2')
+	class Meta(UserCreationForm.Meta):
+		model = User
+		fields = ('email', 'first_name', 'last_name', 'username', 'birthday', 'cellphone')
 
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("El email ya se encuentra registrando, intente nuevamente.")
-        return email
+	def __init__(self, *args, **kwargs):
+		super(UserCreateFormWithEmail, self).__init__(*args, **kwargs)
+		#Modificar en tiempo real
+		self.fields['username'].widget = forms.TextInput(attrs={'class':'form-control mb-2','placeholder':'Nombre de Usuario'})
+
+		self.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control mb-2', 'placeholder':'Dirección de Email'})
+
+		self.fields['cellphone'].widget = forms.TextInput(attrs={'class':'form-control mb-2',' placeholder':'Celular'})
+
+		self.fields['birthday'].widget = forms.DateInput(attrs={'class':'form-control mb-2',' placeholder':'Fecha de Nacimiento'})
+
+		self.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2',' placeholder':'Contraseña'})
+
+		self.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2',' placeholder':'Repite la contraseña'})
+
+
