61809fd88acc6d4d2b6fff6cdf3171fe49bda962
CamiBM <camila140795@gmail.com>
2019-10-24 15:16:54 -0300

    Se actualizo el registerUser.html

--- apps/usuario/forms.py
+++ apps/usuario/forms.py
@@ -7,35 +7,35 @@ from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
 
-    username =forms.EmailField(label="Nombre de Usuario", required=True, widget=forms.EmailInput(
-        attrs={'class':'form-control', 'placeholder':'Escribe tu usuario'}
+    username =forms.CharField(label="Nombre de Usuario:", required=True, widget=forms.TextInput(
+        attrs={'placeholder':'Escribe tu usuario'}
     ), min_length=3, max_length=100) 
     
-    first_name=forms.CharField(label="Nombre", required=True, widget=forms.TextInput(
-        attrs={'class':'form-control', 'placeholder':'Escribe tu Nombre'}
+    first_name=forms.CharField(label="Nombre:", required=True, widget=forms.TextInput(
+        attrs={'class':'form-control', 'placeholder':'Escribe tu nombre'}
     ), min_length=3, max_length=30)
 
-    last_name=forms.CharField(label="Apellido", required=True, widget=forms.TextInput(
-        attrs={'class':'form-control', 'placeholder':'Escribe tu Apellido'}
+    last_name=forms.CharField(label="Apellido:", required=True, widget=forms.TextInput(
+        attrs={'class':'form-control', 'placeholder':'Escribe tu apellido'}
     ), min_length=3, max_length=30)
 
-    email = forms.EmailField(label="Email", required=True, widget=forms.EmailInput(
-        attrs={'class':'form-control', 'placeholder':'Escribe tu Email'}
+    email = forms.EmailField(label="Email:", required=True, widget=forms.EmailInput(
+        attrs={'class':'form-control', 'placeholder':'ejemplo@gmail.com'}
     ), min_length=3, max_length=100)
 
-    cellphone = forms.CharField(label="Celular", required=True, widget=forms.TextInput(
-        attrs={'class':'form-control', 'placeholder':'Escribe tu celular'}
+    cellphone = forms.CharField(label="Celular:", required=True, widget=forms.TextInput(
+        attrs={'class':'form-control', 'placeholder':'3624112233'}
     ), min_length=3, max_length=30)
     
-    birthday = forms.DateField(label="Fecha de Nacimiento", required=True, widget=forms.DateInput(
-      attrs={'class':'form-control', 'rows':2, 'placeholder':'Escribe tu fecha de nacimiento'}  
+    birthday = forms.DateField(label="Fecha de Nacimiento:", required=True, widget=forms.DateInput(
+      attrs={'class':'form-control', 'rows':2, 'placeholder':'dd/mm/aaaa'}  
     ))
 
-    password1 = forms.CharField(required=True, widget=forms.PasswordInput(
+    password1 = forms.CharField(label="Contraseña:", required=True, widget=forms.PasswordInput(
       attrs={'class':'form-control', 'rows':2, 'placeholder':'Escribe tu contraseña'}  
     ), min_length=5, max_length=25)
 
-    password2 = forms.CharField(required=True, widget=forms.PasswordInput(
+    password2 = forms.CharField(label="Confirmar contraseña:", required=True, widget=forms.PasswordInput(
       attrs={'class':'form-control', 'rows':2, 'placeholder':'Repite tu contraseña'}  
     ), min_length=5, max_length=25)
 
