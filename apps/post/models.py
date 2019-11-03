444954b6069b0ae37aaea39d837b47c5b2b19b6a
sandinosanchez <sandino320@gmail.com>
2019-11-03 19:38:08 -0300

    Agregado funcionaldiades del post, crear y listar

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from .utils import TipoDenunciaEnum, TipoPostEnum
 from apps.usuario.models import User
 
-class Post(models.Models):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
+class PostBase(models.Model):
+	created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 	img = models.ImageField(blank=True, null=True)
-	date_time = models.DateTimeField(auto_now_add=True)
-	
+	creation_date = models.DateTimeField(auto_now_add=True)
+	post_type = models.CharField(choices=TipoPostEnum.choices(), max_length=25)
+
+
+class Denuncia(PostBase):
+	tipo = models.CharField(choices=TipoDenunciaEnum.choices(), max_length=25)
+	text = models.CharField(max_length=500)
+
+
+class PostInformativo(PostBase):
+	text = models.CharField(max_length=1000)
+
+
 
 
---
+++ apps/post/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	
+	# Urls to create posts
+	path('create-denuncia/', views.CreateDenuncia.as_view(), name='create-denuncia'),
+	path('create-info/', views.CreatePostInformativo.as_view(), name='create-info'),
+
+	# Urls to lists posts
+	path('denuncia/', views.ListDenuncia.as_view(), name='denuncia-home'),
+	path('info/', views.ListPostInformativo.as_view(), name='info-home'),
+
+]
