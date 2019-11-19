3aaeb0f6fa668506b82a526b97c266488b5f594b
Karina <karysol_92@hotmail.com>
2019-11-19 12:05:40 -0300

    correcciones realizadas

--- apps/post/forms.py
+++ apps/post/forms.py
@@ -8,14 +8,12 @@ class CreateDenuncia(forms.ModelForm):
 	tipo = forms.ChoiceField(choices=TipoDenunciaEnum.choices())
 	text = forms.CharField(max_length=500)
 	# falta definir el titulo para los posts
-
 	class Meta:
 		model = Denuncia
 		fields = ['tipo', 'text', 'img']
 
 class CreatePostInformativo(forms.ModelForm):
 	text = forms.CharField(max_length=1000)
-
 	class Meta:
 		model = PostInformativo
 		fields = ['text', 'img']
