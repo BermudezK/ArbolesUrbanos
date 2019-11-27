7c27d70d5a74a70f30b45acd940d7a9fa206a3db
Raizu548 <rizzoluis53@gmail.com>
2019-11-27 19:04:31 -0300

    as

--- apps/post/views.py
+++ apps/post/views.py
@@ -6,10 +6,9 @@ from django.http import HttpResponseRedirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from .forms import CreateDenunciaForm, CreatePostInformativo, ImagenForm
-from .models import Denuncia, PostInformativo
+from .models import Denuncia, PostInformativo, PostImg
 from apps.arbol.models import Tree
 from apps.usuario.models import User
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 
@@ -41,6 +40,7 @@ def CreateDenunciaa(request):
 				d.titulo = request.POST.get('titulo')
 				d.tipo = request.POST.get('tipo')
 				d.text = request.POST.get('text')
+				d.email = usuario.email
 				d.save()
 
 				denuncia = Denuncia.objects.get(pk = d.pk)
@@ -81,7 +81,6 @@ def CreateDenunciaa(request):
 			redirecion = redirect(reverse('denuncia'))
 			return redirecion
 
-	# Si no es un usuario registrado ejecuta esta funcion
 	else:
 		if request.method == 'GET':
 			form = CreateDenunciaForm()
@@ -95,6 +94,7 @@ def CreateDenunciaa(request):
 				d.titulo = request.POST.get('titulo')
 				d.tipo = request.POST.get('tipo')
 				d.text = request.POST.get('text')
+				d.email = request.POST.get('email')
 				d.save()
 
 				denuncia = Denuncia.objects.get(pk = d.pk)
@@ -138,82 +138,15 @@ def CreateDenunciaa(request):
 	return render(request,'core/create-denuncia.html',{'form':form,'form2':form2})
 
 
-def CreatePostInfor(request):
+class CreatePostInformativo(CreateView):
+	form_class = CreatePostInformativo
+	template_name = 'core/create-info.html'
+	success_utl = reverse_lazy('home')
 
-	if request.user.is_authenticated:
-		usuario = User.objects.get(email = request.user)
-
-		if request.method == 'GET':
-			form = CreatePostInformativo()
-			form2 = ImagenForm()
-		else:
-			form = CreatePostInformativo(request.POST)
-			form2 = ImagenForm(request.POST, request.FILES)
-			print(form.is_valid())
-			print(form2.is_valid())
-
-			if form.is_valid() and form2.is_valid():
-				d = form.save(commit = False)
-				d.created_by = usuario
-				d.titulo = request.POST.get('titulo')
-				d.text = request.POST.get('text')
-				d.post_type = "INFORMATIVO"
-				d.save()
-
-				informacion = PostInformativo.objects.get(pk = d.pk)
-				if request.POST.get('img') != '':	
-					form2 = ImagenForm(request.POST, request.FILES)
-					i = form2.save(commit = False)
-					i.post = informacion
-					i.img = request.FILES.get('img')
-					i.save()
-
-				if request.POST.get('img2') != '':
-					form2 = ImagenForm(request.POST, request.FILES)
-					i = form2.save(commit = False)
-					i.post = informacion
-					i.img = request.FILES.get('img2')
-					i.save()
-
-				if request.POST.get('img3') != '':
-					form2 = ImagenForm(request.POST, request.FILES)
-					i = form2.save(commit = False)
-					i.post = informacion
-					i.img = request.FILES.get('img3')
-					i.save()
-
-				if request.POST.get('img4') != '':
-					form2 = ImagenForm(request.POST, request.FILES)
-					i = form2.save(commit = False)
-					i.post = informacion
-					i.img = request.FILES.get('img4')
-					i.save()
-
-				if request.POST.get('img5') != '':
-					form2 = ImagenForm(request.POST, request.FILES)
-					i = form2.save(commit = False)
-					i.post = informacion
-					i.img = request.FILES.get('img5')
-					i.save()
-
-			redirecion = redirect(reverse('info'))
-			return redirecion
-	else:
-		redirecion = redirect(reverse('login'))
-		return redirecion
-
-	return render(request,'core/create-info.html',{'form':form,'form2':form2})
-
-
-# class CreatePostInformativo(CreateView):
-# 	form_class = CreatePostInformativo
-# 	template_name = 'core/create-info.html'
-# 	success_utl = reverse_lazy('home')
-
-# 	def form_valid(self, form):
-# 		obj = form.save(commit=False)
-# 		obj.created_by = self.request.user
-# 		return super(CreatePostInformativo, self).form_valid(form)
+	def form_valid(self, form):
+		obj = form.save(commit=False)
+		obj.created_by = self.request.user
+		return super(CreatePostInformativo, self).form_valid(form)
 
 
 def listar(request):
@@ -221,26 +154,39 @@ def listar(request):
 	de = Denuncia.objects.order_by('-creation_date')
 	lp = PostInformativo.objects.order_by('-creation_date')
 	p = Tree.objects.get(pk=1)
+	img = PostImg.objects.distinct('post_id')
 	context['arbol'] = p
 	context['denuncias'] = de
 	context['informativo'] = lp
+	context['imagen'] = img
 
 	return render(request, 'core/home.html', context)
 
 
+def ListarPostDenuncia(request):
+	context = {}
+	objdenuncia = Denuncia.objects.order_by('-creation_date')
+	p = Tree.objects.get(pk=1)
+	img = PostImg.objects.distinct('post_id')
+	context['denuncias'] = objdenuncia
+	context['imagen'] = img
 
-class ListPostDenuncia(ListView):
-	model = Denuncia
-	template_name = 'core/denuncias.html'
+	return render(request, 'core/denuncias.html', context)
 
-	def get_queryset(self):
-		result = Denuncia.objects.order_by('-creation_date')
-		return result
 
-	def get_context_data(self, *args, **kwargs):
-		context = super(ListPostDenuncia, self).get_context_data(*args, **kwargs)
-		context['arbol'] = Tree.objects.get(pk=1)
-		return context
+
+# class ListPostDenuncia(ListView):
+# 	model = Denuncia
+# 	template_name = 'core/denuncias.html'
+
+# 	def get_queryset(self):
+# 		result = Denuncia.objects.order_by('-creation_date')
+# 		return result
+
+# 	def get_context_data(self, *args, **kwargs):
+# 		context = super(ListPostDenuncia, self).get_context_data(*args, **kwargs)
+# 		context['arbol'] = Tree.objects.get(pk=1)
+# 		return context
 
 # ACA HAY QUE AVERIGUAR COMO HACER PARA ENVIAR EL NUMERO TOTAL DE ARBOLES
 class ListPostInformativo(ListView):
